7f37bc38f81fda7621553e035e3c3534e176c3fc
gaozhenfeng <1937982680@qq.com>
2022-03-03 09:09:11 +0800

    升级版本

--- tio-study-client/src/main/java/com/lgy/client/HelloClientStarter.java
+++ tio-study-client/src/main/java/com/lgy/client/HelloClientStarter.java
@@ -1,65 +1,65 @@
-package com.lgy.client;
-
-import com.lgy.common.Const;
-import com.lgy.common.HelloPacket;
-import org.tio.client.AioClient;
-import org.tio.client.ClientChannelContext;
-import org.tio.client.ClientGroupContext;
-import org.tio.client.ReconnConf;
-import org.tio.client.intf.ClientAioHandler;
-import org.tio.client.intf.ClientAioListener;
-import org.tio.core.Aio;
-import org.tio.core.Node;
-
-import java.util.Scanner;
-
-
-/**
- * Created by fengch on 2018-03-16.
- */
-public class HelloClientStarter {
-    //服务器节点
-    public static Node serverNode = new Node(Const.SERVER, Const.PORT);
-
-    //handler, 包括编码、解码、消息处理
-    public static ClientAioHandler aioClientHandler = new HelloClientAioHandler();
-
-    //事件监听器，可以为null，但建议自己实现该接口，可以参考showcase了解些接口
-    public static ClientAioListener aioListener = null;
-
-    //断链后自动连接的，不想自动连接请设为null
-    private static ReconnConf reconnConf = new ReconnConf(5000L);
-
-    //一组连接共用的上下文对象
-    public static ClientGroupContext clientGroupContext = new ClientGroupContext(aioClientHandler, aioListener, reconnConf);
-
-    public static AioClient aioClient = null;
-    public static ClientChannelContext clientChannelContext = null;
-
-    /**
-     * 启动程序入口
-     */
-    public static void main(String[] args) throws Exception {
-        clientGroupContext.setHeartbeatTimeout(Const.TIMEOUT);
-        aioClient = new AioClient(clientGroupContext);
-        clientChannelContext = aioClient.connect(serverNode);
-        //连上后，发条消息玩玩
-
-        Scanner input = new Scanner(System.in);
-        String val = null;
-        do{
-            System.out.print("请输入：");
-            val = input.next();
-
-            send(val);
-
-        }while(!val.equals("quert"));
-        input.close(); // 关闭资源
-    }
-
-    private static void send(String msg) throws Exception {
-        HelloPacket packet = new HelloPacket();
-        packet.setBody(msg.getBytes(HelloPacket.CHARSET));
-        Aio.send(clientChannelContext, packet);
-    }
-}
+//package com.lgy.client;
+//
+//import com.lgy.common.Const;
+//import com.lgy.common.HelloPacket;
+//import org.tio.client.AioClient;
+//import org.tio.client.ClientChannelContext;
+//import org.tio.client.ClientGroupContext;
+//import org.tio.client.ReconnConf;
+//import org.tio.client.intf.ClientAioHandler;
+//import org.tio.client.intf.ClientAioListener;
+//import org.tio.core.Aio;
+//import org.tio.core.Node;
+//
+//import java.util.Scanner;
+//
+//
+///**
+// * Created by fengch on 2018-03-16.
+// */
+//public class HelloClientStarter {
+//    //服务器节点
+//    public static Node serverNode = new Node(Const.SERVER, Const.PORT);
+//
+//    //handler, 包括编码、解码、消息处理
+//    public static ClientAioHandler aioClientHandler = new HelloClientAioHandler();
+//
+//    //事件监听器，可以为null，但建议自己实现该接口，可以参考showcase了解些接口
+//    public static ClientAioListener aioListener = null;
+//
+//    //断链后自动连接的，不想自动连接请设为null
+//    private static ReconnConf reconnConf = new ReconnConf(5000L);
+//
+//    //一组连接共用的上下文对象
+//    public static ClientGroupContext clientGroupContext = new ClientGroupContext(aioClientHandler, aioListener, reconnConf);
+//
+//    public static AioClient aioClient = null;
+//    public static ClientChannelContext clientChannelContext = null;
+//
+//    /**
+//     * 启动程序入口
+//     */
+//    public static void main(String[] args) throws Exception {
+//        clientGroupContext.setHeartbeatTimeout(Const.TIMEOUT);
+//        aioClient = new AioClient(clientGroupContext);
+//        clientChannelContext = aioClient.connect(serverNode);
+//        //连上后，发条消息玩玩
+//
+//        Scanner input = new Scanner(System.in);
+//        String val = null;
+//        do{
+//            System.out.print("请输入：");
+//            val = input.next();
+//
+//            send(val);
+//
+//        }while(!val.equals("quert"));
+//        input.close(); // 关闭资源
+//    }
+//
+//    private static void send(String msg) throws Exception {
+//        HelloPacket packet = new HelloPacket();
+//        packet.setBody(msg.getBytes(HelloPacket.CHARSET));
+//        Aio.send(clientChannelContext, packet);
+//    }
+//}
--- tio-study-example/src/main/java/com/lgy/client/HelloClientAioHandler.java
+++ tio-study-example/src/main/java/com/lgy/client/HelloClientAioHandler.java
@@ -1,21 +1,19 @@
 package com.lgy.client;
 
+import java.nio.ByteBuffer;
+
 import com.lgy.common.HelloPacket;
 import org.tio.client.intf.ClientAioHandler;
 import org.tio.core.ChannelContext;
-import org.tio.core.GroupContext;
+import org.tio.core.TioConfig;
 import org.tio.core.exception.AioDecodeException;
 import org.tio.core.intf.Packet;
-
-import java.nio.ByteBuffer;
-
 /**
- * Created by fengch on 2018-03-16.
+ * 
+ * @author tanyaowu
  */
 public class HelloClientAioHandler implements ClientAioHandler {
     private static HelloPacket heartbeatPacket = new HelloPacket();
-
-
     /**
      * 解码：把接收到的ByteBuffer，解码成应用可以识别的业务消息包
      * 总的消息结构：消息头 + 消息体
@@ -23,21 +21,17 @@ public class HelloClientAioHandler implements ClientAioHandler {
      * 消息体结构：   对象的json串的byte[]
      */
     @Override
-    public HelloPacket decode(ByteBuffer buffer, ChannelContext channelContext) throws AioDecodeException {
-        int readableLength = buffer.limit() - buffer.position();
+    public HelloPacket decode(ByteBuffer buffer, int limit, int position, int readableLength, ChannelContext channelContext) throws AioDecodeException {
         //收到的数据组不了业务包，则返回null以告诉框架数据不够
         if (readableLength < HelloPacket.HEADER_LENGHT) {
             return null;
         }
-
         //读取消息体的长度
         int bodyLength = buffer.getInt();
-
         //数据不正确，则抛出AioDecodeException异常
         if (bodyLength < 0) {
             throw new AioDecodeException("bodyLength [" + bodyLength + "] is not right, remote:" + channelContext.getClientNode());
         }
-
         //计算本次需要的数据长度
         int neededLength = HelloPacket.HEADER_LENGHT + bodyLength;
         //收到的数据是否足够组包
@@ -56,7 +50,6 @@ public class HelloClientAioHandler implements ClientAioHandler {
             return imPacket;
         }
     }
-
     /**
      * 编码：把业务消息包编码为可以发送的ByteBuffer
      * 总的消息结构：消息头 + 消息体
@@ -64,31 +57,27 @@ public class HelloClientAioHandler implements ClientAioHandler {
      * 消息体结构：   对象的json串的byte[]
      */
     @Override
-    public ByteBuffer encode(Packet packet, GroupContext groupContext, ChannelContext channelContext) {
+    public ByteBuffer encode(Packet packet, TioConfig tioConfig, ChannelContext channelContext) {
         HelloPacket helloPacket = (HelloPacket) packet;
         byte[] body = helloPacket.getBody();
         int bodyLen = 0;
         if (body != null) {
             bodyLen = body.length;
         }
-
         //bytebuffer的总长度是 = 消息头的长度 + 消息体的长度
         int allLen = HelloPacket.HEADER_LENGHT + bodyLen;
         //创建一个新的bytebuffer
         ByteBuffer buffer = ByteBuffer.allocate(allLen);
         //设置字节序
-        buffer.order(groupContext.getByteOrder());
-
+        buffer.order(tioConfig.getByteOrder());
         //写入消息头----消息头的内容就是消息体的长度
         buffer.putInt(bodyLen);
-
         //写入消息体
         if (body != null) {
             buffer.put(body);
         }
         return buffer;
     }
-
     /**
      * 处理消息
      */
@@ -100,15 +89,12 @@ public class HelloClientAioHandler implements ClientAioHandler {
             String str = new String(body, HelloPacket.CHARSET);
             System.out.println("收到消息：" + str);
         }
-
         return;
     }
 
-    /**
-     * 此方法如果返回null，框架层面则不会发心跳；如果返回非null，框架层面会定时发本方法返回的消息包
-     */
+
     @Override
-    public HelloPacket heartbeatPacket() {
+    public Packet heartbeatPacket(ChannelContext channelContext) {
         return heartbeatPacket;
     }
 }
